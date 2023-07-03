ca351902f2b4301589535186d1fb8329e3b5d739
rayhan60611 <pecon.alok@gmail.com>
2023-07-03 14:32:31 +0600

    bootstrap 5 added and data showed as a bootstarp card

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -5,12 +5,14 @@ const Country = (props) => {
   const { region, area, name, flags, population } = props.country;
   return (
     <div className="card">
-      <img className="card-img-top" src={flags.svg} alt="" />
+      <img className="card-img-top" height={"100%"} src={flags.svg} alt="" />
       <div className="card-body text-start">
-        <h3 className="text-danger">Country: {name.common}</h3>
+        <h4 className="text-danger">Country: {name.common}</h4>
         <h4 className="text-primary">Region: {region}</h4>
-        <h5 className="text-info">Population: {population}</h5>
-        <h5 className="text-success">Area: {area}</h5>
+        <h5 className="text-info">Population: {population} Crore</h5>
+        <h5 className="text-success">
+          Area: {area} Km<sup>2</sup>
+        </h5>
       </div>
     </div>
   );
